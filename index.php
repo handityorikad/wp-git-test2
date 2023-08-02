70a515aa76fa810d476e3e311b5e39cc84fbf65f
handityo rikad <handityorikad@gmail.com>
2023-08-02 22:58:16 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>hellow ordl</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quis esse mollitia consequatur dolores? Natus, quibusdam ipsam! Numquam officia hic culpa assumenda ut suscipit distinctio, placeat repudiandae error nobis quo.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
